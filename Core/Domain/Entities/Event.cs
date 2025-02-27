047bd4312c75651d3508bf6885d3c2768b6548cd
Heley Danylo <78755636+StoRmGC228@users.noreply.github.com>
2025-02-27 12:19:25 +0200

    Implement basic entities

---
+++ Core/Domain/Entities/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    using Microsoft.VisualBasic;
+
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int? Price { get; set; }
+        public string? Format  { get; set; }
+        public string Game { get; set; }
+        public DateAndTime EventStartTime { get; set; }
+        public virtual List<User> Subscribers { get; set; }
+
+    }
+}
