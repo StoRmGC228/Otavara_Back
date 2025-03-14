846416860c3116869bf47e969df4d80d5c832998
Юра <@DESKTOP-NALH133>
2025-03-14 13:44:55 +0200

    Implement GoodRepository

--- Core/Domain/Entities/Good.cs
+++ Core/Domain/Entities/Good.cs
@@ -13,6 +13,7 @@ namespace Domain.Entities
         public string Description { get; set; }
         public double Price { get; set; }
         public int QuantityInStock { get; set; }
+        public DateTime CreatedAt { get; set; }
         public virtual List<BookedGood> Bookers { get; set; }
     }
 }
