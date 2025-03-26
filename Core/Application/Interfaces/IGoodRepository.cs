7aa342463bf08ad3ba1ad4e2411a280ec8c37db6
Юра <@DESKTOP-NALH133>
2025-03-26 14:57:45 +0200

    fix some bug

--- Core/Application/Interfaces/IGoodRepository.cs
+++ Core/Application/Interfaces/IGoodRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Application.Interfaces
 {
     public interface IGoodRepository : IBaseRepository<Good>
-    {
+    {   
         Task<IEnumerable<Good>> GetAllSortedByNameAsync(bool ascending); // true - orderby, false - orderby descending
         Task<IEnumerable<Good>> GetAllSortedByQuantityAsync(bool ascending);// true - from largest to smallest quantity
         Task<IEnumerable<Good>> GetAllSortedByTimeAsync(bool ascending); // true - orderby(from smalest), false - orderby descending
--- Core/Domain/Entities/Good.cs
+++ Core/Domain/Entities/Good.cs
@@ -1,15 +1,13 @@
 ﻿namespace Domain.Entities;
 
-namespace Domain.Entities
+public class Good : BaseEntity
 {
-    public class Good:BaseEntity
-    {
-        public Guid CustomerId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public double Price { get; set; }
-        public int QuantityInStock { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public virtual List<BookedGood> Bookers { get; set; }
-    }
+    public Guid Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public int QuantityInStock { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public virtual List<BookedGood> Bookers { get; set; }
 }
