ee0441a69a4acf4465f6f3a50106c775bd9c0b2e
Олег Гайдучок <oleh.haiduchok@lnu.edu.ua>
2025-03-30 16:12:31 +0300

    Implemented: IRequestedCardService RequstedCardService RequestedCardController

---
+++ Presentation/API/Controllers/RequestedCardController.cs
@@ -0,0 +1,137 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RequestedCardController : ControllerBase
+{
+    private readonly IRequestedCardService _requestedCardService;
+    public RequestedCardController(IRequestedCardService requestedCardService)
+    {
+        _requestedCardService = requestedCardService;
+    }
+
+    //BaseService methods
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateRequestedCard(Guid id, [FromBody] RequestedCard updatedRequestedCard)
+    {
+        if (id != updatedRequestedCard.Id)
+        {
+            return BadRequest();
+        }
+        var existingRequestedCard = await _requestedCardService.GetByIdAsync(id);
+        if (existingRequestedCard == null)
+        {
+            return NotFound();
+        }
+        var result = await _requestedCardService.UpdateAsync(updatedRequestedCard);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteRequestedCard(Guid id)
+    {
+        var existingRequestedCard = await _requestedCardService.GetByIdAsync(id);
+        if (existingRequestedCard == null)
+        {
+            return NotFound();
+        }
+        await _requestedCardService.DeleteAsync(id);
+        return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllRequestedCards()
+    {
+        var requestedCards = await _requestedCardService.GetAllAsync();
+        return Ok(requestedCards);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetRequestedCardById(Guid id)
+    {
+        var requestedCard = await _requestedCardService.GetByIdAsync(id);
+        if (requestedCard == null)
+        {
+            return NotFound();
+        }
+        return Ok(requestedCard);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateRequestedCard([FromBody] RequestedCard newRequestedCard)
+    {
+        var result = await _requestedCardService.AddAsync(newRequestedCard);
+        return CreatedAtAction(nameof(GetRequestedCardById), new { id = result.Id }, result);
+    }
+
+    //RequestedCardService methods : Injected from RequestedCardService
+    [HttpGet("{code}")]
+    public async Task<IActionResult> GetRequestedCardByCode(string code)
+    {
+        var requestedCard = await _requestedCardService.GetByCodeAsync(code);
+        if (requestedCard == null)
+        {
+            return NotFound();
+        }
+        return Ok(requestedCard);
+    }
+    
+    [HttpGet("{link}")]
+    public async Task<IActionResult> GetRequestedCardByLink(string link)
+    {
+        var requestedCard = await _requestedCardService.GetByLinkAsync(link);
+        if (requestedCard == null)
+        {
+            return NotFound();
+        }
+        return Ok(requestedCard);
+    }
+    
+    [HttpGet("{requesterId}")]
+    public async Task<IActionResult> GetRequestedCardByRequesterId(Guid requesterId)
+    {
+        var requestedCards = await _requestedCardService.GetByRequesterIdAsync(requesterId);
+        return Ok(requestedCards);
+    }
+    
+    [HttpGet("{eventId}")]
+    public async Task<IActionResult> GetRequestedCardByEventId(Guid eventId)
+    {
+        var requestedCards = await _requestedCardService.GetByEventIdAsync(eventId);
+        return Ok(requestedCards);
+    }
+
+    //RequestedCardService methods : Custom methods
+    [HttpPost]
+    public async Task<IActionResult> CreateRequestedCard(Guid requesterId, Guid eventId, string link, string code, int number)
+    {
+        var result = await _requestedCardService.AddRequestedCardAsync(requesterId, eventId, link, code, number);
+        return CreatedAtAction(nameof(GetRequestedCardById), new { id = result.Id }, result);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CancelRequestedCard(Guid requestedCardId)
+    {
+        var result = await _requestedCardService.CancelRequestedCardAsync(requestedCardId);
+        return Ok(result);
+    }
+    
+    [HttpGet("{code}")]
+    public async Task<IActionResult> IsRequestedCardExists(string code)
+    {
+        var result = await _requestedCardService.IsRequestedCardExistsAsync(code);
+        return Ok(result);
+    }
+    
+    [HttpGet("{code}")]
+    public async Task<IActionResult> IsRequestedCardUsed(string code)
+    {
+        var result = await _requestedCardService.IsRequestedCardUsedAsync(code);
+        return Ok(result);
+    }
+}
+
