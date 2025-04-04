be9461e55be9729c9859fe600f57641cb61fe042
Heley Danylo <78755636+StoRmGC228@users.noreply.github.com>
2025-04-04 13:33:08 +0300

    Add Json Converters

---
+++ Presentation/API/Configurations/ApiConfigurations.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Configurations;
+
+using Converters;
+using Microsoft.AspNetCore.Http.Json;
+
+public static class ApiConfigurations
+{
+    public static IServiceCollection AddApiConfigurations(this IServiceCollection services,
+        IConfiguration configurations)
+    {
+        services.Configure<JsonOptions>(opt =>
+        {
+            opt.SerializerOptions.Converters.Add(new DateTimeConverter());
+            opt.SerializerOptions.Converters.Add(new TimeOnlyConverter());
+            opt.SerializerOptions.Converters.Add(new DateOnlyConverter());
+        });
+        return services;
+    }
+}
