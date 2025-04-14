f089cc6ef8efb3ccabd812d66da3027314a4272c
Heley Danylo <78755636+StoRmGC228@users.noreply.github.com>
2025-04-14 15:56:59 +0300

    Update db conection for render

--- Presentation/API/Configurations/InfrastructureConfiguration.cs
+++ Presentation/API/Configurations/InfrastructureConfiguration.cs
@@ -4,6 +4,7 @@ using Application.Interfaces;
 using Infrastructure.Configurations;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 public static class InfrastructureConfiguration
 {
@@ -11,7 +12,34 @@ public static class InfrastructureConfiguration
         IConfiguration configuration)
     {
         //var connectionString = configuration.GetValue<string>("ConnectionString");
-        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+        //var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+        var rawConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+                                  ?? Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        if (string.IsNullOrEmpty(rawConnectionString))
+            throw new InvalidOperationException("Database connection string is not set in environment variables.");
+
+        string connectionString = rawConnectionString;
+
+        // Якщо строка у форматі URI (як на Render), перетвори її
+        if (connectionString.StartsWith("postgres://"))
+        {
+            var uri = new Uri(connectionString);
+            var userInfo = uri.UserInfo.Split(':');
+
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = uri.Host,
+                Port = uri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = uri.AbsolutePath.Trim('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+
+            connectionString = builder.ConnectionString;
+        }
         services.AddDbContext<OtavaraDbContext>(options => options.UseNpgsql(connectionString));
         services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
         services.AddScoped<IUserRepository, UserRepository>();
