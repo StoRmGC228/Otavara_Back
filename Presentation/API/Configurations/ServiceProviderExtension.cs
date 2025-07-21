dcded574f6c5694f6d8baf325019caa4e5169cf7
Skip1L <96205978+Skip1L@users.noreply.github.com>
2025-07-21 16:56:24 +0300

    forget to add in previous commit

---
+++ Presentation/API/Configurations/ServiceProviderExtension.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Hangfire;
+
+namespace API.Configurations;
+
+public static class ServiceProviderExtensions
+{
+    public static void AddRecurringJobs(this IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var provider = scope.ServiceProvider;
+
+        var logger = provider.GetRequiredService<ILoggerFactory>()
+            .CreateLogger("RecurringJobs");
+
+        logger.LogInformation("Registering recurring Hangfire jobs...");
+
+        RecurringJob.AddOrUpdate<IAnnouncementRepository>(
+            "cleanup-overdue-announcements",
+            repo => repo.DeleteOverdueAnnouncements(),
+            Cron.Daily,
+            new RecurringJobOptions
+            {
+                TimeZone = TimeZoneInfo.Local, // або UTC, залежно від логіки
+            });
+
+        logger.LogInformation("Recurring job 'cleanup-overdue-announcements' registered.");
+    }
+}
